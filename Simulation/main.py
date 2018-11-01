9b040c90c39928d905efb5212573fda01e4129f0
ziyue wang <wangziyue@hotmail.co.uk>
2018-11-01 23:47:13 +0000

    Got sphere working

--- Simulation/main.py
+++ Simulation/main.py
@@ -6,7 +6,6 @@ import numpy as np
 import object
 
 
-
 #Probe params
 xRange = [i for i in range(0,1000)]        #100x100 scan for probe, across 100x100
 yRange = [i for i in range(0,1000)]
@@ -16,29 +15,35 @@ yRange = [i for i in range(0,1000)]
 cLMax = 500     #Cube length max
 
 #Generate random parameters of the cube
-aRand = np.random.rantint(0,cLMax)
-bRand = np.random.rantint(0,cLMax)
-cRand = np.random.rantint(0,cLMax)
+aRand = np.random.randint(0,cLMax)
+bRand = np.random.randint(0,cLMax)
+cRand = np.random.randint(0,cLMax)
 
 #Generate random numbers for start position
-xStartRandom = np.random.randint(0,max)
+xPosRandom = np.random.randint(cLMax/2,max(xRange)-cLMax/2)
+yPosRandom = np.random.randint(cLMax/2,max(yRange)-cLMax/2)
 
-cube = object.cuboid("tungsten",0,0,0,xStart,yStart,xRange,yRange,aRand,bRand,cRand)
+#cube = object.cuboid("tungsten",0,0,0,xPosRandom,yPosRandom,xRange,yRange,aRand,bRand,cRand)
 
-cube.calcThickness()
+#cube.calcThickness()
 #print(cube.thicknessMatrix)
 
+rRand = np.random.randint(0,cLMax)
+sphere = object.sphere("xs",0,0,0,xPosRandom,yPosRandom,xRange,yRange,rRand)
+print(rRand)
+
+sphere.calcThicknessMatrix()
 
 
 
 #plotting of STEM prototype (no noise yet)
 
-A = cube.thicknessMatrix
+A = sphere.thicknessMatrix
 
 def addPoissonNoise(Matrix):
     for i in range(len(xRange)):
         for u in range (len(yRange)):
-            noise = np.random.poisson(5.0)
+            noise = np.random.poisson(rRand)            #How big should the noise be? Any ideas?
             Matrix[i][u] += noise
 
 addPoissonNoise(A)            
--- Simulation/object.py
+++ Simulation/object.py
@@ -7,24 +7,25 @@ Created on Thu Nov  1 14:40:06 2018
 """
 #This file is made for the objects, starting with base class and children
 import numpy as np
+import math
 
 
 class objectType:
 	#Alpha Beta Gamma are the tilts or angles to the x,y,z planes respectively.
-    def __init__(self,material,alpha,beta,gamma,xStart,yStart,xProbeRange,yProbeRange):
+    def __init__(self,material,alpha,beta,gamma,xPos,yPos,xProbeRange,yProbeRange):
         self.material = material
         self.alpha = alpha
         self.beta = beta
         self.gamma = gamma
-        self.xStart = xStart
-        self.yStart = yStart
+        self.xPos = xPos
+        self.yPos = yPos
         self.xProbeRange = xProbeRange
         self.yProbeRange = yProbeRange
 #Add future function here to calculate tilt, just stating variables for now
 
 class cuboid(objectType):
-    def __init__(self,material,alpha,beta,gamma,xStart,yStart,xProbeRange,yProbeRange,a,b,c):
-        super().__init__(material,alpha,beta,gamma,xProbeRange,yProbeRange)
+    def __init__(self,material,alpha,beta,gamma,xPos,yPos,xProbeRange,yProbeRange,a,b,c):
+        super().__init__(material,alpha,beta,gamma,xPos,yPos,xProbeRange,yProbeRange)
         self.a = a		#a b c are length scales of a cuboid, arbitary
         self.b = b
         self.c = c
@@ -42,14 +43,39 @@ class cuboid(objectType):
 
 
 class sphere(objectType):
-	def __init(self,material,alpha,beta,gamma,xStart,yStart,xProbeRange,yProbeRange,r):
-		super().__init__(material,alpha,beta,gamma,xProbeRange,yProbeRange)		#Consider removing the angles since sphere doesn't tilt - may be a waste of memory
-		self.r = r		#radius	
+    def __init__(self,material,alpha,beta,gamma,xPos,yPos,xProbeRange,yProbeRange,r):
+        super().__init__(material,alpha,beta,gamma,xPos,yPos,xProbeRange,yProbeRange)		#Consider removing the angles since sphere doesn't tilt - may be a waste of memory
+        self.r = r		#radius
+        
+        #Not including z position as it is trivial in STEM. The thickness is still the same regardless of z position.
+    def calcThickness(self,xProbePos,yProbePos):
+        zSqr = self.r**2- (xProbePos-self.xPos)**2 - (yProbePos-self.yPos)**2
+       #print(str(self.r) + " " + str(xProbePos) + " " + str(self.xPos) + " " + str(yProbePos) + " " + str(self.yPos))
+       #print(zSqr)
+        if zSqr > 0:                            #Really lazy check. What essentially happened is we've placed the sphere in a box, and the function below checks 
+            z = float(math.sqrt(zSqr))          #if the probe is within the region of the box. Then it tries to probe it. But at the corners of the box, the sphere
+            return abs(z*2)                     #doesn't touch it. Hence we got negative numbers. This check is a lazy check, correct later, checks for negatives
+        else:                                   #and just returns as 0. Probably a better and more effective way than this, but for now keep it at it is.
+            return 0
+        
+        
+        
+        
+    def calcThicknessMatrix(self):
+        self.thicknessMatrix = np.zeros((len(self.xProbeRange),len(self.yProbeRange)))
+        for i in range(len(self.xProbeRange)):              #For the range of the probe
+            for u in range (len(self.yProbeRange)):
+                if i in range(self.xPos-self.r,self.xPos+self.r+1) and u in range(self.yPos-self.r,self.yPos+self.r+1):         #this confirms that it's in the range
+                    self.thicknessMatrix[i][u] = self.calcThickness(i,u)
+                    
+        
+        
+        
 #Maybe change this to spherical polars later on? and the cylinder to cylindrical polars? Would that have any benefit? 
 
 class cylinder(objectType):
-	def __init__(self,material,alpha,beta,gamma,xStart,yStart,xProbeRange,yProbeRange,r,l):
-		super().__init__(material,alpha,beta,gamma,xProbeRange,yProbeRange)
+	def __init__(self,material,alpha,beta,gamma,xPos,yPos,xProbeRange,yProbeRange,r,l):
+		super().__init__(material,alpha,beta,gamma,xPos,yPos,xProbeRange,yProbeRange)
 		self.r = r	
 		self.l = l		#r for radius, l for length (across cylinder)
 
